1f4ad3e223962055980169f2ea39ee543818af3e
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-06 14:28:59 -0500

    doing controller

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -2,3 +2,22 @@ const express = require('express')
 
 
 const clientApi = require('../models/clientModel.js')
+
+
+
+const clientRouter = express.Router()
+
+
+
+
+//get all 
+
+clientRouter.get('/', (req, res) => {
+    clientApi.getAllClients()
+      .then((allClients) => {
+        console.log(allClients)
+        res.json(allClients)
+  
+      })
+  })
+  
