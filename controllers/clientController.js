1f61b6c6c1ecf6af4583d0c35e04c6ad54376603
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-06 15:15:54 -0500

    still on controller for client

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -14,10 +14,31 @@ const clientRouter = express.Router()
 
 clientRouter.get('/', (req, res) => {
     clientApi.getAllClients()
-      .then((allClients) => {
-        console.log(allClients)
-        res.json(allClients)
-  
+        .then((allClients) => {
+            console.log(allClients)
+            res.json(allClients)
+
+        })
+})
+
+
+// get one
+clientRouter.get('/:id', (req, res) => {
+    clientApi.getSingleClient(req.params.id)
+      .then((singleClient) => {
+        res.json(singleClient)
       })
   })
-  
+
+
+
+// create client 
+clientRouter.post('/', (req, res) => {
+    clientApi.createClient(req.body)
+        .then((createdClient) => {
+            res.json(createdClient)
+        })
+})
+
+
+
