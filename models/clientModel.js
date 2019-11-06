f69fdbb28729a4edf11f3fb13a3eba3d7f0820e4
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-06 14:19:03 -0500

    made client model

--- models/clientModel.js
+++ models/clientModel.js
@@ -7,8 +7,36 @@ const ClientSchema = new mongoose.Schema({
     city: String,
     state: String,
     email: String
-  })
+})
 
-const ClientCollection = mongoose.model('Client',ClientSchema)
+const ClientCollection = mongoose.model('Client', ClientSchema)
+
+// get all 
+const getAllClients = () => {
+    return ClientCollection.find({})
+}
 
 
+//get one
+const getSingleClient = (id) => {
+    return ClientCollection.findById(id)
+}
+
+
+// create
+const createClient = (clientData) => {
+    return ClientCollection.create(clientData)
+}
+
+//delete
+const deleteClient = (id) => {
+    return ClientCollection.deleteOne({ _id: id })
+}
+
+
+module.exports ={
+    getAllClients,
+    getSingleClient,
+    createClient,
+    deleteClient
+}
