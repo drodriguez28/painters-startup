bdf47e9a5a114360a3fe3e2dcf65e01837e8278f
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-08 11:11:50 -0500

    jsx html painter

--- client/src/components/Createpainter.jsx
+++ client/src/components/Createpainter.jsx
@@ -30,6 +30,11 @@ export default class Createpainter extends Component {
 
         }
 
+        addNewPainter = (event) => {
+            event.preventDefault();
+
+        }
+
 
 
 
@@ -38,7 +43,57 @@ export default class Createpainter extends Component {
 
         return (
             <div>
+                <form onSubmit={this.addNewPainter}>
+                <div>
+                    <input
+                        name="name"
+                        type="text"
+                        placeholder="name"
+                        value={history.state.newPainter.name}
+                        onChange={this.handleNewPainter}
+                    />
+                </div>
+                <div>
+                    <input
+                        name="city"
+                        type="text"
+                        placeholder="city"
+                        value={history.state.newPainter.city}
+                        onChange={this.handleNewPainter}
+                    />
+                </div>
 
+                <div>
+                    <input
+                        name="age"
+                        type="text"
+                        placeholder="age"
+                        value={history.state.newPainter.age}
+                        onChange={this.handleNewPainter}
+                    />
+                </div>
+
+
+                <div>
+                    <input
+                        name="email"
+                        type="text"
+                        placeholder="email"
+                        value={history.state.newPainter.email}
+                        onChange={this.handleNewPainter}
+                    />
+                </div>
+
+
+
+                <div>
+                        <input
+                            type="submit"
+                            value="Create New Painter"
+                        />
+                    </div>
+
+            </form>
             </div>
         )
     }
