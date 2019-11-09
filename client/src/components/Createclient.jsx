c3fdb8d95ebe828f10932b57c26eeee77248902d
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-09 10:32:53 -0500

    createclient

--- client/src/components/Createclient.jsx
+++ client/src/components/Createclient.jsx
@@ -1,36 +1,37 @@
 import React, { Component } from 'react'
 import axios from "axios"
-import {Redirect} from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
 
 
 
 
 export default class Createclient extends Component {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    state = {
+        redirect: false,
+        newClient: {
+            name:"",
+            typeofjob:"",
+            city:"",
+            state:"",
+            email:""
+
+        },
+        createdClientId: null
+    }
+
+
+
+
+
+
+
+
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
