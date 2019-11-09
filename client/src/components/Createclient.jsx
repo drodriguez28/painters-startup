bbbc905bcbaa8a3e101d9769b88adf4e1cec652b
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-11-09 10:26:52 -0500

    added create client

---
+++ client/src/components/Createclient.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import axios from "axios"
+import {Redirect} from 'react-router-dom'
+
+
+
+
+
+export default class Createclient extends Component {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
